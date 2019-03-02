93bdbc02a3931157c72036718e51948d6da33a50
YangHuiTri <913603802@qq.com>
2019-03-02 17:15:51 +0800

    更新源码
    之前代码上传错了，重新上传

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,21 @@
+<?php
+//声明命名空间
+namespace Admin\Controller;
+//引入父类
+use Think\Controller;
+//声明当前类并继承父类
+class IndexController extends Controller{
+
+	//index方法
+	public function index(){
+		//展示模板
+		$this->display();
+	}
+
+	//home方法
+	public function home(){
+		//展示模板
+		$this->display();
+	}
+
+}
---
+++ Application/Runtime/Cache/Admin/8c4c5101828303b31401d041aef4a352.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1><?php echo ($arr['1']); ?></h1>
+</body>
+</html>
